3d5c3ca0c57775cf256f1af71b6a052ecd8bd32e
tmolteno <tim@elec.ac.nz>
2016-05-14 12:18:21 +1200

    Use plate simulator for the moment

--- prop/prop.py
+++ prop/prop.py
@@ -11,8 +11,8 @@ import stl_tools
 import motor_model
 
 from design_parameters import DesignParameters
-from foil_simulator import XfoilSimulatedFoil as FoilSimulator
-#from foil_simulator import PlateSimulatedFoil as FoilSimulator
+#from foil_simulator import XfoilSimulatedFoil as FoilSimulator
+from foil_simulator import PlateSimulatedFoil as FoilSimulator
 from scipy.interpolate import PchipInterpolator
 
 class Prop:
