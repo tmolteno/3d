c964b5280b822d66010b5549156139d3800b99cb
Lord-Treepa <lordtreepa@gmail.com>
2016-06-24 15:36:27 +1200

    Changed wing area code and added more photos

--- sycamore/wing.py
+++ sycamore/wing.py
@@ -1,19 +1,29 @@
 import cv2
 import numpy as np 
 from matplotlib import pyplot as plt
-
+ihg = raw_input("Num of seed: ")
 # read image
-img = cv2.imread('1cm.jpg',0)
-ret,thresh = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY_INV)
+hlf = ihg + "Crop.jpg"
+cm = cv2.imread('1cmCrop.jpg',0)
+img = cv2.imread(hlf, 0)
+ret,thresh = cv2.threshold(cm, 100, 255, cv2.THRESH_BINARY_INV)
+ret,image = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY_INV)
 height, width = img.shape
 
 cv2.namedWindow('img', cv2.WINDOW_NORMAL)
 cv2.namedWindow('thresh', cv2.WINDOW_NORMAL)
 
-cv2.imshow('img', img)
+cv2.imshow('img', image)
 cv2.imshow('thresh', thresh)
+cm = cv2.countNonZero(thresh)
+bob = cv2.countNonZero(image)
+bob = float(bob)
+bob1 = bob / cm
+print cm
+print bob
+print bob1
 
-cv2.waitKey(0)
+cv2.waitKey(500)
 
 #print "height and width : ",height, width
 #size = img.size
