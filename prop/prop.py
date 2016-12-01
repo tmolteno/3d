Added correct formula for a hovering prop, assuming that the velocity is half way between the wash speed and the zero speed upwind

prop/prop.py
@@ -48,7 +48,8 @@ class Prop:
         rps = rpm / 60.0
         return np.sqrt((r**2*rho)**(1/3)*(rps*tau)**(2/3)/(r**2*rho))
 
-
+    def get_air_density(self):
+        return 1.225 # kg m^{-3}
     
     def get_max_chord(self,r, twist):
         ''' Allowed chord as a function of radius (m) 
@@ -409,7 +410,7 @@ if __name__ == "__main__":
     optimum_torque, optimum_rpm = m.get_Qmax(param.motor_volts)
     power = m.get_Pmax(param.motor_volts)
     print("Optimum Motor Torque %f at %f RPM, power=%f" % (optimum_torque, optimum_rpm, power))
-    v = p.get_air_velocity_at_prop(optimum_torque, optimum_rpm)
+    v = p.get_air_velocity_at_prop(optimum_torque, optimum_rpm)/2
     print("Airspeed at propellers (hovering): %f" % (v))
     param.forward_airspeed = v
 