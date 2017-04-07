2af94d4b9b6faf247b450ac2c91df97cbf58d6ba
Tim Molteno <tim@molteno.net>
2017-04-07 22:57:40 +1200

    Added referential integrity to allow simulations to be deleted, and clean up polars

--- prop/foil_simulator.sql
+++ prop/foil_simulator.sql
@@ -1,5 +1,15 @@
+CREATE TABLE foil(
+    id integer PRIMARY KEY AUTOINCREMENT,
+    hash varchar);
+
+CREATE TABLE simulation(
+    id integer PRIMARY KEY AUTOINCREMENT,
+    foil_id int REFERENCES foil ON DELETE CASCADE, 
+    reynolds float, 
+    mach float);
+
 CREATE TABLE  polar (
-    sim_id int, 
+    sim_id int REFERENCES simulation ON DELETE CASCADE, 
     alpha float, 
     cl float, 
     cd, 
@@ -8,12 +18,4 @@ CREATE TABLE  polar (
     Top_Xtr, 
     Bot_Xtr);
     
-CREATE TABLE foil(
-    id integer PRIMARY KEY AUTOINCREMENT,
-    hash varchar);
     
-CREATE TABLE simulation(
-    id integer PRIMARY KEY AUTOINCREMENT,
-    foil_id int, 
-    reynolds float, 
-    mach float);
